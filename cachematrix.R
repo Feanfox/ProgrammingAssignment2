2f9fa3e2b384e79b6aa817527b2575e56e74b76a
Feanfox <acebloodhounds@gmail.com>
2014-12-21 14:39:49 +0300

    PA2
    My solution of the Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## This function creates a matrix which is able to cache an inverted itself
 
 makeCacheMatrix <- function(x = matrix()) {
-
+inver <- NULL
+create <- function(z) {
+x <<- z
+inver <<- NULL
+}
+obtain <- function() {
+x
+}
+create_i <- function(i) {
+inver <<- i
+}
+obtain_i <- function() {
+inver
+}
+list(create = create,
+obtain = obtain,
+create_i = create_i,
+obtain_i = obtain_i)
 }
 
-
-## Write a short comment describing this function
+## This function calculates the inverse or just shows it, taking it from the cache
+## if the inverse was calculated before
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+inver <- x$obtain_i()
+if(!is.null(inver)) {
+return(inver)
 }
+mx <- x$obtain()
+inver <- solve(mx, ...)
+x$create_i(inver)
+return(inver)
+}
+
